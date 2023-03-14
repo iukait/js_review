37c940ec6e33d2900e8049f3899a55e0ae017620
julia_celarscaia <julia.celarscaia@gmail.com>
2023-03-14 08:13:07 -0700

    lesson 2 and 3 and array were added

---
+++ loops.js
@@ -0,0 +1,29 @@
+// let dayDistance = 3;
+// let nightDown = -2;
+
+// let distance = 0;
+// let days = 0;
+// let wall = 5;
+
+// if (distance += dayDistance <= wall) {
+//     day++;
+// 
+// }
+
+
+let login = true;
+let a = 4;
+
+let course1 = 'js';
+let course2 = 'java';
+let course = course1;
+let region = 'USA';
+
+if (login == true) {
+    if (course == course1) {
+        console.log(`welcome`)
+    }
+    else {
+        conlole.log(`Go to java`)
+    }
+}
